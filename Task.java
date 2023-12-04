613259bf484b139566494045c3f541526e6208b2
Bohdan Datsiv <b.datsiv@cargo.external>
2023-12-04 16:34:39 +0200

    first commit

---
+++ Task.java
@@ -0,0 +1,8 @@
+package newModules.module1.homeWork10;
+
+public class Task {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Java");
+    }
+}
